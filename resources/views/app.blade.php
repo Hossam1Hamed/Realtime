ed6106884ae3c88bd04315421cfa7e93c4160ef3
Hossam Hamed <h.hamed@neoxero.com>
2023-09-06 01:55:12 +0300

    connect to pusher server --- all edits in .env file and app.blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+        <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+        <script !src="">
+            Pusher.logToConsole = true;
+            var pusher = new Pusher('ba721db5b8ca98882137', {
+                cluster: 'eu'
+            });
+        </script>
+    </body>
+</html>
